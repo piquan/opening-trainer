74a0478612fffcba31fc14cd6b16ecb251b102c0
Joel Ray Holveck <joelh@piquan.org>
2023-08-03 01:22:22 -0700

    Increase the build chunk size warning
    Pretty much, all the JavaScript is required from the initial page load.  I'm not too worried about it being 1 MB.
    This doesn't include Stockfish, which is loaded and started asynchronously.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        // For now, we let the JS reach 1MB without a warning.  I
+        // don't really see a point in doing any manual chunking.
+        chunkSizeWarningLimit: 1024,
+    },
     //BASE
 })
