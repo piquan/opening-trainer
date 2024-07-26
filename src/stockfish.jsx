e1afdd99cb874f973b2d6c2b35ce17560496d9e6
Joel Ray Holveck <joelh@piquan.org>
2024-07-25 21:55:42 -0700

    Refactor the StockfishManager.

--- src/stockfish.jsx
+++ src/stockfish.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Chess } from 'chess.js'
 
 // The multithreaded versions requires SharedArrayBuffer objects.
@@ -10,7 +11,7 @@ import { Chess } from 'chess.js'
 // threads.  For this application, that's not a big deal, though.
 const kEngine = 'stockfish-nnue-16-single.js';
 
-export function StockfishManager() {
+export class StockfishManager extends EventTarget {
     // Overview: The state is maintained as uci (startup), isready
     // (waiting to get to a state where we can send new positions),
     // and run (evaluating or idle).  We maintain the most recent
@@ -37,32 +38,42 @@ export function StockfishManager() {
     // Instead, we symlink to the required files as static files,
     // and just load them here.  That also helps with not having
     // useless reloads every time I rebuild.
-    const worker = (typeof Worker === "undefined" ? null :
-                    new Worker(kEngine));
+
+    #worker = (typeof Worker === "undefined" ? null : new Worker(kEngine));
 
     // State: uci, isready, run, idle
-    let state = 'uci';
+    #state = 'uci';
+
     // These are the values we want to have; we'll send them to Stockfish
     // once it's initialized, or reports it's ready.
-    let newPos = 'startpos';
-    let newEvalDepth = 0;
+    #newPos = 'startpos';
+    #newEvalDepth = 0;
+
     // sideFactor is 1 if the engine is evaluating white, -1 if
     // black.  This is because eval output is from the current side's
     // POV, while it's presented as + for white and - for black.
     // It's updated when a new "go" command is sent.
-    let sideFactor = null;
-    // Users subscribe by setting adding callbacks to oninfo.  They
-    // also can read the latest info.
-    let info = {};
-    const oninfo = [];
+    #sideFactor = null;
 
-    function sendCommand (msg) {
-        console.debug("[%s] stockfish> %s", state, msg);
-        worker.postMessage(msg);
+    // Users can subscribe by setting an addEventListener for the "info"
+    // event on this object.  They'll get an event with the info in
+    // e.detail.  However, because we're using useSyncExternalStore,
+    // our users actually use `subscribe` and `getInfo`.
+    #info = {};
+
+    constructor() {
+        super();
+        this.#worker.onmessage = (e => this.#handleMessage(e.data));
+        this.#sendCommand("uci");
     }
 
-    function handleMessage (msg) {
-        console.debug("[%s] stockfish< %s", state, msg);
+    #sendCommand (msg) {
+        console.debug("[%s] stockfish> %s", this.#state, msg);
+        this.#worker?.postMessage(msg);
+    }
+
+    #handleMessage (msg) {
+        console.debug("[%s] stockfish< %s", this.#state, msg);
 
         // When we get "info" lines, we just handle them without bothering with
         // the state machine.
@@ -74,39 +85,40 @@ export function StockfishManager() {
             }
             if (typeof infoMatch.groups.mate !== "undefined") {
                 const rawMate = Number.parseInt(infoMatch.groups.mate, 10);
-                newInfo.mate = sideFactor * rawMate;
+                newInfo.mate = this.#sideFactor * rawMate;
             }
             if (typeof infoMatch.groups.cp !== "undefined") {
                 const rawCp = Number.parseInt(infoMatch.groups.cp, 10);
-                newInfo.pawns = sideFactor * rawCp * 0.01;
+                newInfo.pawns = this.#sideFactor * rawCp * 0.01;
             }
             if (Object.keys(newInfo).length !== 0) {
-                info = newInfo;
-                oninfo.forEach(c => c(newInfo));
+                this.#info = newInfo;
+                const e = new CustomEvent("info", {detail: newInfo});
+                this.dispatchEvent(e);
             }
             return;
         }
 
-        if (state === "uci") {
+        if (this.#state === "uci") {
             if (msg !== "uciok") {
                 return;
             }
-            sendCommand('setoption name Use NNUE value true');
-            sendCommand('isready');
-            state = 'isready';
-        } else if (state === 'isready') {
+            this.#sendCommand('setoption name Use NNUE value true');
+            this.#sendCommand('isready');
+            this.#state = 'isready';
+        } else if (this.#state === 'isready') {
             // We transition to the 'isready' state when we have new
             // data to send.
             if (msg !== 'readyok') {
                 return;
             }
-            if (newEvalDepth > 0) {
-                sendCommand("position " + newPos);
-                sendCommand(`go depth ${newEvalDepth}`);
+            if (this.#newEvalDepth > 0) {
+                this.#sendCommand("position " + this.#newPos);
+                this.#sendCommand(`go depth ${this.#newEvalDepth}`);
 
                 // Parse the position, so we can load it into chess.js
                 // to find whose turn it is.  That will be the sideFactor.
-                const posMatch = newPos.match(/^\s*(?:(?<startpos>startpos)|fen\s+(?<fen>[^m]+))(?:\s+moves\s+(?<moves>.*))?$/)
+                const posMatch = this.#newPos.match(/^\s*(?:(?<startpos>startpos)|fen\s+(?<fen>[^m]+))(?:\s+moves\s+(?<moves>.*))?$/)
                 const chess = new Chess();
                 if (typeof posMatch.groups.fen !== "undefined") {
                     chess.load(posMatch.groups.fen);
@@ -116,64 +128,68 @@ export function StockfishManager() {
                     const movelist = posMatch.groups.moves.trim().split(/\s+/);
                     movelist.forEach(m => chess.move(m));
                 }
-                sideFactor = chess.turn() === "w" ? 1 : -1;
+                this.#sideFactor = chess.turn() === "w" ? 1 : -1;
             }
             // Even if we're not actually running, we put our state machine
             // in the 'run' state, since our transition table is the same.
-            state = 'run';
-        } else if (state === "run") {
+            this.#state = 'run';
+        } else if (this.#state === "run") {
             // In run state, we don't care about anything other than the info
             // lines we already checked for.
         }
     }
 
-    this.close = function () {
-        sendCommand("quit");
-    };
-    this.setPosDepth = function (position, evalDepth) {
+    setPosDepth(position, evalDepth) {
         // The position needs to be what you'd have after a UCI
         // 'position' command, such as 'startpos' or
         // 'fen blah/blah moves e24' or whatever.
-        if (position === newPos && evalDepth === newEvalDepth) {
+        if (position === this.#newPos && evalDepth === this.#newEvalDepth) {
             // We've sent, or arranged to send, this information already.
             // (This may happen on rerenders or something.)
             return;
         }
-        newPos = position;
-        newEvalDepth = evalDepth;
+        this.#newPos = position;
+        this.#newEvalDepth = evalDepth;
 
-        if (state === "run") {
+        if (this.#state === "run") {
             // Even if we've finished the entire eval depth, or haven't sent
             // a "go" (because we're not evaluating), we can still send
             // a "stop" command without problems.
-            sendCommand("stop");
+            this.#sendCommand("stop");
         }
         // Make sure that we've gotten to a ready state.  Once we have,
         // the state machine will update it with our new position.
         // Don't send a new isready if we're already in a state that will
         // bring us there.
-        if (state !== "uci" && state !== "isready") {
-            sendCommand("isready");
-            state = "isready";
+        if (this.#state !== "uci" && this.#state !== "isready") {
+            this.#sendCommand("isready");
+            this.#state = "isready";
         }
-    };
-    this.subscribe = function (callback) {
-        oninfo.push(callback);
-        return () => {
-            const index = oninfo.indexOf(callback);
-            if (index > -1) {
-                oninfo.splice(index, 1);
-            }
-        };
-    };
-    this.getInfo = function () {
-        return info;
-    };
-
-    // We've now set ourselves, so we can (in the right order) start
-    // the worker.
-    if (worker !== null) {
-        worker.onmessage = (e => handleMessage(e.data));
-        sendCommand("uci");
     }
+
+    // These are defined as arrow functions so that `this` is appropriately
+    // set, even if they are passed as function objects to
+    // useSyncExternalStore.  Normally, `const a = foo.method;`
+    // doesn't retain `this` when `a()` is called.  But with this syntax,
+    // it does.
+    subscribe = callback => {
+        const newcb = e => callback();
+        this.addEventListener("info", newcb);
+        const unsubscribe = () => this.removeEventListener("info", newcb);
+        return unsubscribe;
+    }
+    getInfo = () => this.#info;
+
+    close() {
+        this.#sendCommand("quit");
+    }
+}
+
+export function useStockfish(lanHistory, depth) {
+    const mgr = React.useMemo(() => new StockfishManager(), []);
+    React.useEffect(() => {
+        mgr.setPosDepth("startpos moves " + lanHistory.join(" "), depth);
+    }, [lanHistory, depth, mgr]);
+    const info = React.useSyncExternalStore(mgr.subscribe, mgr.getInfo);
+    return info;
 }
